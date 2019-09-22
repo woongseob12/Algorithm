0a1ac1930008e77e0a9b24448a00fdd6a9623845
woongseob12 <woongseob12@naver.com>
2019-09-22 15:47:05 +0900

    곱셈

---
+++ 1-12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	int first,second,third,forth;
+	scanf("%d %d",&a,&b);
+	first = a*(b%10);
+	second = a*((b%100)-(b%10));
+	third = a*(b-(b%100));
+	forth = first + second + third;
+	printf("%d\n",first);
+	printf("%d\n",second);
+	printf("%d\n",third);
+	printf("%d\n",forth);
+	return 0;
+}
