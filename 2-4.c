50dd050bd8b62160df0b90d0be2fd0140da16545
woongseob12 <woongseob12@naver.com>
2019-09-24 14:05:10 +0900

    알람 시계

--- 2-4.c
+++ 2-4.c
@@ -5,10 +5,14 @@ int main()
 	int H,M;
 	scanf("%d %d",&H,&M);
 	M = M-45;
-	if (M<0)
+	if(M<0)
 	{
 		H = H-1;
 		M = 60 + M;
+	}
+	if(H<0)
+	{
+		H = 24 + H;
 	}
 	printf("%d %d\n",H,M);
 	return 0;
