9e95d11d6c27900bf7ce6acd50bf9e0d9473cc9c
woongseob12 <woongseob12@naver.com>
2019-09-26 10:42:28 +0900

    최소,최대

--- 5-1.c
+++ 5-1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	int num[1000000];
+	int *num;
 	int size;
 	int i,j,temp;
 	scanf("%d",&size);
+	num = (int*)malloc(sizeof(int)*size);
 	for (i = 0;i<size;i++){
 		scanf("%d",&num[i]);
 	}
@@ -20,6 +22,7 @@ int main()
 		}
 	}
 	printf("%d %d\n",num[0],num[size-1]);
+	free(num);
 	return 0;
 }
 
