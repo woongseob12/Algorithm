c7c3965120480b4886bd3eaf73fa4fc293b269ce
woongseob12 <woongseob12@naver.com>
2019-10-02 11:37:54 +0900

    숫자의 개수

--- 5-4.c
+++ 5-4.c
@@ -4,11 +4,22 @@ int main()
 {
 	int a,b,c;
 	int res;
+	int k;
 	int i,j,cal;
 	int num[10] = {0,};
 	scanf("%d %d %d",&a,&b,&c);
 	res = a* b* c;
-	for(i = 10;i < 1000000000; i*=10){
+	if((res >= 1000000)&&(res <=9999999)){
+		num[0] = -2;
+	}
+	else if((res >= 10000000)&&(res <=99999999)){
+		num[0] = -1;
+	}
+	else {
+		k = res /100000000;
+		num[k]++;
+	}	
+	for(i = 10;i <= 1000000000; i*=10){
 		cal = res % i;
 		for(j=0;j<10;j++){
 			if(j == cal*10/i){
