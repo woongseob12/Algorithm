9a08644f364c37f03da2f71c65f19ee8f46e193f
woongseob12 <woongseob12@naver.com>
2019-10-02 16:15:55 +0900

    OX퀴즈

---
+++ 5-7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int many;
+	int i = 0;
+	char ox[80];
+	scanf("%d",&many);
+	while(scanf("%c",&ox[i]!='\0')){
+		scanf("%c",&ox[i]);
+		i++;
+	}
+	whiel(1){
+		printf("%c",ox[i]);
+		if(ox[i] == '\0'){
+			break;
+		}
+	}
+	printf("\n");
+	return 0;
+}
+
