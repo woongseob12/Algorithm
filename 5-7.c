87a9f06f63e7b2145213a83a7df9b79e3a578b0c
woongseob12 <woongseob12@naver.com>
2019-10-10 11:05:13 +0900

    OX퀴즈

--- 5-7.c
+++ 5-7.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	int many;
-	int i = 0;
-	char ox[80];
-	scanf("%d",&many);
-	while(scanf("%c",&ox[i]!='\0')){
-		scanf("%c",&ox[i]);
-		i++;
+	int row,i,j,sum,score;
+	char **ox;
+	int *result;
+	scanf("%d",&row);
+	ox=(char**)malloc(sizeof(char)*row);
+	result=(int*)malloc(sizeof(int)*row);
+	for(i = 0; i<row;i++){
+		ox[i] = (char*)malloc(sizeof(char*)*80);
 	}
-	whiel(1){
-		printf("%c",ox[i]);
-		if(ox[i] == '\0'){
-			break;
+	for(i = 0; i < row ; i++){
+		scanf("%s",ox[i]);
+	}
+	for (i = 0; i< row; i++){
+		sum = 0;
+		score = 1;
+		for (j = 0; j<80;j++){
+			 if(ox[i][j] == 'O'){
+				  sum += score;
+				  score ++;
+				  }
+			 else if(ox[i][j] == 'X'){
+				 score = 1;
+			 }
+			 else{
+				 ;
+			 }
 		}
+		result[i] = sum;
 	}
-	printf("\n");
+	for (i = 0; i <row;i++){
+		printf("%d\n",result[i]);
+	}
+	free(result);
+	for(i = 0;i<row;i++){
+		free(ox[i]);
+	}
 	return 0;
 }
 
