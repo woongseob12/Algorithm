758d5f7531f8f39969260b5e947ed62062e08424
woongseob12 <woongseob12@naver.com>
2019-10-16 12:55:20 +0900

    평균은 넘겠지

--- 5-8.c
+++ 5-8.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
 	int caseNum, avg, sum = 0;
 	int i,cnt=0;
-	int *p;
+	int p[1000];
 	double per;
 
 	scanf("%d",&caseNum);
-	p=(int*)malloc(sizeof(int)*caseNum);
-	for(i = 0; i< caseNum; i++)
-	{
+	for(i = 0; i< caseNum; i++){
 		scanf("%d",&p[i]);
 	}
 	for(i = 0; i < caseNum; i++)
@@ -27,7 +23,6 @@ int main()
 	}
 	per = (double)cnt/(double)caseNum;
 	printf("%.3f%%\n",per*100);
-	free(p);
 	return 0;
 }
 
