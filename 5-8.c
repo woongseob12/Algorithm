36fbcc10dba6f4c5aa0bde78de9c3f2d10d14ed6
woongseob12 <woongseob12@naver.com>
2019-10-31 13:01:43 +0900

    평균은 넘겠지

--- 5-8.c
+++ 5-8.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-	int caseNum, avg, sum = 0;
-	int i,cnt=0;
-	int p[1000];
-	double per;
-
-	scanf("%d",&caseNum);
-	for(i = 0; i< caseNum; i++){
-		scanf("%d",&p[i]);
-	}
-	for(i = 0; i < caseNum; i++)
+	int testCase,aryNum;
+	int i,cnt,studentNum;
+	int sum,score,avg;
+	int *aP;
+	double *p;
+	scanf("%d",&testCase);
+	aryNum = testCase;
+	p = (double*)malloc(sizeof(double)*testCase);
+	while(testCase>0)
 	{
-		sum+=p[i];
-	}
-	avg = sum/caseNum;
-	for(i = 0; i < caseNum; i++)
-	{
-		if(avg < p[i]){
-			cnt++;
+		scanf("%d",&studentNum);
+		sum = 0;
+		cnt = 0;
+		aP = (int*)malloc(sizeof(int)*studentNum);
+		for(i = 0;i<studentNum;i++){
+			scanf("%d",&aP[i]);
+			sum +=aP[i];
 		}
+		avg = sum/studentNum;
+		for(i=0;i<studentNum;i++){
+			if (aP[i] > avg){
+				cnt++;
+			}
+		}
+		p[aryNum-testCase] =(double)cnt/(double)studentNum;
+		testCase--;
 	}
-	per = (double)cnt/(double)caseNum;
-	printf("%.3f%%\n",per*100);
+	free(aP);
+	for (i = 0;i < aryNum;i++){
+		printf("%.3lf%%\n",p[i]*100);
+	}
+	free(p);
 	return 0;
 }
+
+
+
+
+
+
 
 
 
