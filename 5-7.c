3c436e9f3540821fca44d31a665e0aa80974518f
woongseob12 <woongseob12@naver.com>
2019-11-12 14:22:06 +0900

    OX퀴즈

--- 5-7.c
+++ 5-7.c
@@ -3,42 +3,34 @@
 
 int main()
 {
-	int row,i,j,sum,score;
-	char **ox;
-	int *result;
-	scanf("%d",&row);
-	ox=(char**)malloc(sizeof(char)*row);
-	result=(int*)malloc(sizeof(int)*row);
-	for(i = 0; i<row;i++){
-		ox[i] = (char*)malloc(sizeof(char*)*80);
-	}
-	for(i = 0; i < row ; i++){
-		scanf("%s",ox[i]);
-	}
-	for (i = 0; i< row; i++){
+	int i,j,sum,score;
+	char ox[80];
+	int testCaseNum;
+	scanf("%d",&testCaseNum);
+	int *p;
+	p = (int*)malloc(sizeof(int)*testCaseNum);
+
+	for(i = 0 ; i < testCaseNum; i++){
+		scanf("%s",ox);
 		sum = 0;
-		score = 1;
-		for (j = 0; j<80;j++){
-			 if(ox[i][j] == 'O'){
-				  sum += score;
-				  score ++;
-				  }
-			 else if(ox[i][j] == 'X'){
-				 score = 1;
-			 }
-			 else{
-				 ;
-			 }
+		score = 0;
+		for(j = 0; j < 80 ; j++){
+			if (ox[j] =='\0'){
+				break;}
+			else if (ox[j] == 'O'){
+				score++;
+				sum += score;
+			}
+			else if (ox[j] == 'X'){
+				score = 0;
+			}
 		}
-		result[i] = sum;
+		p[i] = sum;	
 	}
-	for (i = 0; i <row;i++){
-		printf("%d\n",result[i]);
+	for(i = 0; i < testCaseNum; i++){
+		printf("%d\n",p[i]);
 	}
-	free(result);
-	for(i = 0;i<row;i++){
-		free(ox[i]);
-	}
+	free(p);
 	return 0;
 }
 
