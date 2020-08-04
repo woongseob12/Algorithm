f8b996acdcfac7581429a2564520b702973227fa
woongseob12 <55429912+woongseob12@users.noreply.github.com>
2020-08-04 23:54:28 +0900

    별 찍기 - 13

--- new5-4.cpp
+++ new5-4.cpp
@@ -4,17 +4,18 @@ using namespace std;
 int main()
 {
 	int line;
-	cin>>line;
-	for(int i=0; i< line*2 - 1; i++){
-		if(i<=line){
-			for(int j= 0; j<i;j++){
-				cout<<"*";
+	cin >> line;
+	for (int i = 1; i <= line * 2 - 1; i++) {
+		if (i <= line) {
+			for (int j = 0; j < i; j++) {
+				cout << "*";
 			}
 		}
-		else{
-			for(int j = i; j < line*2 -1; j++){
-				cout<<"*";
+		else {
+			for (int j = i; j <= line * 2 - 1; j++) {
+				cout << "*";
 			}
 		}
+		cout << endl;
 	}
 }
