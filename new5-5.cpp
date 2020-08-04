edc02dc76d62f28eae64d347188d2be18f196b34
woongseob12 <woongseob12@naver.com>
2020-08-05 00:08:35 +0900

    별 찍기 -9

---
+++ new5-5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int line;
+	int cnt;
+	cin >> line;
+	for (int i = 1; i <= line*2 - 1; i++) {
+		if (i <= line) {
+			for (int j = i; j<= 1; j--){
+				cout<<" ";
+			}
+			for (int j = i; j <= line*2 -1; j++) {
+				cout << "*";
+			}
+			for (int j = i; j<=1; j--){
+				cout<<" ";
+			}
+		}
+		else {
+			cnt = i - line;
+			for (int j = i; j < line*2 -1; j++){
+				cout<<" ";
+			}
+			for (int j = 0; j <cnt*2 +1; j++) {
+				cout << "*";
+			}
+			for (int j = i; j < line*2 -1; j++){
+				cout<<" ";
+			}
+		}
+		cout << endl;
+	}
+}
