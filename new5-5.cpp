e15633ce68b7ee8e80d01079f9377d32d4f1c384
woongseob12 <55429912+woongseob12@users.noreply.github.com>
2020-08-05 00:21:25 +0900

    별 찍기 - 9

--- new5-5.cpp
+++ new5-5.cpp
@@ -6,28 +6,28 @@ int main()
 	int line;
 	int cnt;
 	cin >> line;
-	for (int i = 1; i <= line*2 - 1; i++) {
+	for (int i = 1; i <= line * 2 - 1; i++) {
 		if (i <= line) {
-			for (int j = i; j<= 1; j--){
-				cout<<" ";
+			for (int j = i; j > 1; j--) {
+				cout << " ";
 			}
-			for (int j = i; j <= line*2 -1; j++) {
+			for (int j = i*2 -1; j <= line * 2 - 1; j++) {
 				cout << "*";
 			}
-			for (int j = i; j<=1; j--){
-				cout<<" ";
+			for (int j = i; j > 1; j--) {
+				cout << " ";
 			}
 		}
 		else {
 			cnt = i - line;
-			for (int j = i; j < line*2 -1; j++){
-				cout<<" ";
+			for (int j = i; j < line * 2 - 1; j++) {
+				cout << " ";
 			}
-			for (int j = 0; j <cnt*2 +1; j++) {
+			for (int j = 0; j < cnt * 2 + 1; j++) {
 				cout << "*";
 			}
-			for (int j = i; j < line*2 -1; j++){
-				cout<<" ";
+			for (int j = i; j < line * 2 - 1; j++) {
+				cout << " ";
 			}
 		}
 		cout << endl;
