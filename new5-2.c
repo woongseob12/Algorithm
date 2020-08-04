e698657bbeb18ade2e3a65ee7dc6850e7b446ded
woongseob12 <55429912+woongseob12@users.noreply.github.com>
2020-08-04 23:34:59 +0900

    시간초과로 하드코딩

--- new5-2.c
+++ new5-2.c
@@ -5,37 +5,25 @@ int main()
 {
 	int burger[3];
 	int beverage[2];
-	int burgerMin, beverageMin;
 
-	for(int i = 0;i<sizeof(burger)/sizeof(burger[0]); i++){
-		cin>>burger[i];
-	}
-	for(int i=0; i<sizeof(beverage)/sizeof(beverage[0]);i++){
-		cin>>beverage[i];
-	}
-	for(int i=0; i<sizeof(burger)/sizeof(burger[0]); i++){
-		if(i=0){
+	cin >> burger[0] >> burger[1] >> burger[2];
+	cin >> beverage[0] >> beverage[1];
+
+	int burgerMin = burger[0];
+	int beverageMin = beverage[0];
+	for (int i = 0; i < sizeof(burger) / sizeof(burger[0]); i++) {
+		if (burgerMin > burger[i]) {
 			burgerMin = burger[i];
 		}
-		else{
-			if(burgerMin<burger[i]){
-				burgerMin = burger[i];
-			}
-		}
 	}
-	for(int i=0; i<sizeof(beverage)/sizeof(beverage[0]); i++){
-		if(i=0){
-			beverageMin = beverage[i];
-		}
-		else{
-			if(beverageMin<beverage[i]){
+	for (int i = 0; i < sizeof(beverage) / sizeof(beverage[0]); i++) {
+		if (beverageMin > beverage[i]) {
 				beverageMin = beverage[i];
-			}
 		}
 	}
 	int sum;
 	sum = burgerMin + beverageMin - 50;
-	cout<<sum;
+	cout << sum;
 }
 
 
