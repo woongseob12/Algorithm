0d1b38fe150e1fd54b9a59dbb5c53a6a2adf8af7
woongseob12 <55429912+woongseob12@users.noreply.github.com>
2020-09-25 14:06:08 +0900

    15-1/ 동전0
    1. index값 런타임 에러 안나게 생각하면서 해야함(while문 (~ && index< n))

--- 그리디 알고리즘/11047.cpp
+++ 그리디 알고리즘/11047.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
-bool cmp(pair<int, int> a, pair<int, int> b);
+int money[10];
 
 int main()
 {
-	int n;
-	cin >> n;
-
-	int start, end;
-	vector<pair<int, int>> time;
+	int n, k;
+	cin >> n >> k;
+	
 	for (int i = 0; i < n; i++) {
-		cin >> start >> end;
-		time.push_back(make_pair(start, end));
-	}
-	sort(time.begin(), time.end(), cmp);
-
-	int endTime = time[0].second, cnt = 1;
-	for (int next = 1; next < n; next++) {
-		if (endTime <= time[next].first) {
-			endTime = time[next].second;
-			cnt++;
-		}
+		cin >> money[i];
 	}
 
+	int index = 0;
+	while (money[index] < k && index < n) {
+		index++;
+	}
+	index--;
+
+	int cnt = 0;
+	for (int i = index; i >= 0; i--) {
+		cnt += k / money[i];
+		k %= money[i];
+	}
 	cout << cnt << endl;
-}
-
-bool cmp(pair<int, int> a, pair<int, int> b)
-{
-	if (a.second == b.second) {
-		return a.first < b.first;
-	}
-	return a.second < b.second;
-}
+} 
