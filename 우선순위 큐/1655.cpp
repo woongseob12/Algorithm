5dd77936953f5aaecaba5d567c006cf12aa1a8a4
woongseob12 <55429912+woongseob12@users.noreply.github.com>
2020-10-08 15:02:18 +0900

    21-4/ 가운데를 말해요
    1. 최소 힙, 최대 힙 두가지 사용
    2. 값을 넣은 후 top()값을 통해 올바르게 들어간지 확인
    3. 올바르지 않으면 최대 힙, 최소 힙의 top()값 swap해주기

---
+++ 우선순위 큐/1655.cpp
@@ -0,0 +1,41 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	priority_queue<int> maxHeap;
+	priority_queue<int, vector<int>, greater<int>> minHeap;
+	
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		int val;
+		cin >> val;
+		if (i % 2 == 1) {
+			minHeap.push(val);
+		}
+		else {
+			maxHeap.push(val);
+		}
+
+		if (i >=2 && minHeap.top() < maxHeap.top()) {
+			int maxTemp = maxHeap.top(), minTemp = minHeap.top();
+			maxHeap.pop();
+			minHeap.pop();
+			maxHeap.push(minTemp);
+			minHeap.push(maxTemp);
+		}
+		if (i % 2 == 1) {
+			cout << minHeap.top() << '\n';
+		}
+		else {
+			cout << minHeap.top() << '\n';
+		}
+	}
+}
