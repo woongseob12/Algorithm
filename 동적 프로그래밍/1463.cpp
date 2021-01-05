207d2ddafad18904c2dd8e0e83e1946bbfbbf78d
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-01-05 20:20:26 +0900

    1로만들기 - 재채점
    1. DP를 이용한 풀이
    - 1부터 X까지 값을 최소값으로 채워나감

--- 동적 프로그래밍/1463.cpp
+++ 동적 프로그래밍/1463.cpp
@@ -2,27 +2,23 @@
 #include <algorithm>
 using namespace std;
 
-int ans[1000001];
+int X;
+int dp[1000001];
 
-int main()
-{
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-	int N;
-	cin >> N;
-
-	ans[2] = 1;
-	ans[3] = 1;
-
-	for (int i = 4; i <= N; i++) {
+	cin >> X;
+	for (int i = 2; i <= X; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0) {
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
 		if (i % 3 == 0) {
-			ans[i] = min(ans[i - 1] + 1,ans[i / 3] + 1);
-		}
-		else if (i % 2 == 0) {
-			ans[i] = min(ans[i - 1] + 1, ans[i / 2] + 1);
-		}
-		else {
-			ans[i] = ans[i - 1] + 1;
+			dp[i] = min(dp[i], dp[i / 3] + 1);
 		}
 	}
-	cout << ans[N] << endl;
+	cout << dp[X];
 }
