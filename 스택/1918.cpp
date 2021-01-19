d37b786585c1f5cc0d12abcbbf3b7b957ca59314
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-01-19 23:04:24 +0900

    후위 표기식
    1. 스택을 이용한 풀이
    - 알파벳은 그냥 출력
    - 그 외 연산 기호는 조건에 따라 분류
    - ')' 일 경우 '('가 나올때까지 스택의 기호들을 pop(이때, 비어있는 스택을 검사하지 않기 위해 !op.empty()조건 추가)
    - '(' 일 경우 스택에 넣음
    - '+', '-' 일 경우 연산자 우선순위가 낮으므로 기존 스택에 있는 것들을 다 출력 및 pop한 후 집어 넣음
    - '*', '/'일 경우 연산자 우선순위가 높으므로 같은 우선 순위인 '*','/'는 출력 및 pop하고 '+', '-', '(' 는 그대로 스택에 저장
    - 모든 계산이 끝난 후 연산자가 들어있는 스택이 빌때까지 출력

---
+++ 스택/1918.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	string str;
+	cin >> str;
+	
+	stack<char> op;
+	for (int i = 0; i < (int)str.size(); i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			cout << str[i];
+		}
+		else if (str[i] == '(') {
+			op.push(str[i]);
+		}
+		else if (str[i] == ')') {
+			while (!op.empty() && op.top() !='(') {
+				cout << op.top();
+				op.pop();
+			}
+			op.pop();
+		}
+		else if (str[i] == '+' || str[i] == '-') {
+			while (!op.empty() && op.top() != '(') {
+				cout << op.top();
+				op.pop();
+			}
+			op.push(str[i]);
+		}
+		else {	// *, /
+			while (!op.empty() && (op.top() == '*' || op.top() == '/')) {
+				cout << op.top();
+				op.pop();
+			}
+			op.push(str[i]);
+		}
+	}
+
+	while (!op.empty()) {
+		if (op.top() != '(') {
+			cout << op.top();
+		}
+		op.pop();
+	}
+}
