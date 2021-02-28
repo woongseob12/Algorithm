5cc2e2f2010cbc2279d3a07e813fea7f1a91634f
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-02-28 15:35:47 +0900

    감시
    구현 :
    1. 카메라의 종류와 좌표를 저장
    2. 카메라 별로 다른 방향을 브루트포스 방식으로 설정
    - 종류별로 설정시 틀림

--- 구현/15683.cpp
+++ 구현/15683.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 int N, M, ans = 987654321, numOfCam;
-char map[8][8];
+char map[8][8], arr[8][8];
 vector<pair<pair<int, int>, char>> cam;
-int dir[8];
-int type[8];
+int dir[8], type[8];
 void sel(int cnt);
 void sol();
+void left(int ny, int nx);
+void right(int ny, int nx);
+void up(int ny, int nx);
+void down(int ny, int nx);
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -37,7 +40,7 @@ void sel(int cnt) {
 		sol();
 		return;
 	}
-	// 방향 잡기
+
 	if (type[cnt] == 1) {
 		for (int i = 0; i < 4; i++) {
 			dir[cnt] = i;
@@ -68,276 +71,67 @@ void sel(int cnt) {
 }
 
 void sol() {
-	char arr[8][8];
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			arr[i][j] = map[i][j];
 		}
 	}
 	for (int i = 0; i < (int)cam.size(); i++) {
-		// 1번 카메라
-		if (cam[i].second == '1') {
-			int ny = cam[i].first.first;
-			int nx = cam[i].first.second;
-			if (dir[i] == 0) {
-				while (true) {
-					ny += 1;
-					if (ny >= N || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 1) {
-				while (true) {
-					nx += 1;
-					if (nx >= M || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 2) {
-				while (true) {
-					ny -= 1;
-					if (ny < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 3) {
-				while (true) {
-					nx -= 1;
-					if (nx < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-		}
-		// 2번 카메라
-		else if (cam[i].second == '2') {
-			int ny = cam[i].first.first;
-			int nx = cam[i].first.second;
-			if (dir[i] == 0) {
-				// 상, 하
-				while (true) {
-					ny += 1;
-					if (ny >= N || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				while (true) {
-					ny -= 1;
-					if (ny < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else {
-				// 좌, 우
-				while (true) {
-					nx += 1;
-					if (nx >= M || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				while (true) {
-					nx -= 1;
-					if (nx < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-		}
-		// 3번 카메라
-		else if (cam[i].second == '3') {
-			int ny = cam[i].first.first;
-			int nx = cam[i].first.second;
-			if (dir[i] == 0) {
-				// 상, 우
-				while (true) {
-					ny += 1;
-					if (ny >= N || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					nx += 1;
-					if (nx >= M || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 1) {
-				// 우, 하
-				while (true) {
-					nx += 1;
-					if (nx >= M || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					ny -= 1;
-					if (ny < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 2) {
-				// 하, 좌
-				while (true) {
-					ny -= 1;
-					if (ny < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					nx -= 1;
-					if (nx < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 3) {
-				// 좌, 상
-				while (true) {
-					nx -= 1;
-					if (nx < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					ny += 1;
-					if (ny >= N || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-		}
-		// 4번 카메라
-		else if (cam[i].second == '4') {
-			int ny = cam[i].first.first;
-			int nx = cam[i].first.second;
-			if (dir[i] == 0) {
-				// 상
-				while (true) {
-					ny += 1;
-					if (ny >= N || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				// 우
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					nx += 1;
-					if (nx >= M || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				// 하
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					ny -= 1;
-					if (ny < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 1) {
-				// 우
-				while (true) {
-					nx += 1;
-					if (nx >= M || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				// 하
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					ny -= 1;
-					if (ny < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				// 좌
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					nx -= 1;
-					if (nx < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else if (dir[i] == 2) {
-				// 상
-				while (true) {
-					ny += 1;
-					if (ny >= N || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				// 하
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					ny -= 1;
-					if (ny < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				// 좌
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					nx -= 1;
-					if (nx < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-			}
-			else {
-				// 상
-				while (true) {
-					ny += 1;
-					if (ny >= N || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
-				// 우
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					nx += 1;
-					if (nx >= M || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
+		int ny = cam[i].first.first;
+		int nx = cam[i].first.second;
 
-				// 좌
-				ny = cam[i].first.first;
-				nx = cam[i].first.second;
-				while (true) {
-					nx -= 1;
-					if (nx < 0 || arr[ny][nx] == '6') break;
-					if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-				}
+		if (cam[i].second == '1') {
+			switch (dir[i]) {
+			case 0:
+				up(ny, nx); break;
+			case 1:
+				right(ny, nx); break;
+			case 2:
+				down(ny, nx); break;
+			case 3:
+				left(ny, nx); break;
+			}
+		}
+
+		else if (cam[i].second == '2') {
+			switch (dir[i]) {
+			case 0:
+				up(ny, nx); down(ny, nx); break;
+			case 1:
+				left(ny, nx); right(ny, nx); break;
+			}
+		}
+
+		else if (cam[i].second == '3') {
+			switch (dir[i]) {
+			case 0:
+				up(ny, nx); right(ny, nx); break;
+			case 1:
+				right(ny, nx); down(ny, nx); break;
+			case 2:
+				down(ny, nx); left(ny, nx); break;
+			case 3:
+				left(ny, nx); up(ny, nx); break;
+			}
+		}
+
+		else if (cam[i].second == '4') {
+			switch (dir[i]) {
+			case 0:
+				up(ny, nx); right(ny, nx); down(ny, nx); break;
+			case 1:
+				right(ny, nx); down(ny, nx); left(ny, nx); break;
+			case 2:
+				down(ny, nx); left(ny, nx); up(ny, nx); break;
+			case 3:
+				left(ny, nx); up(ny, nx); right(ny, nx); break;
 			}
 		}
 		else if (cam[i].second == '5') {
-			int ny = cam[i].first.first;
-			int nx = cam[i].first.second;
-			while (true) {
-				ny += 1;
-				if (ny >= N || arr[ny][nx] == '6') break;
-				if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-			}
-			// 우
-			ny = cam[i].first.first;
-			nx = cam[i].first.second;
-			while (true) {
-				nx += 1;
-				if (nx >= M || arr[ny][nx] == '6') break;
-				if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-			}
-			// 하
-			ny = cam[i].first.first;
-			nx = cam[i].first.second;
-			while (true) {
-				ny -= 1;
-				if (ny < 0 || arr[ny][nx] == '6') break;
-				if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-			}
-			// 좌
-			ny = cam[i].first.first;
-			nx = cam[i].first.second;
-			while (true) {
-				nx -= 1;
-				if (nx < 0 || arr[ny][nx] == '6') break;
-				if (arr[ny][nx] == '0') arr[ny][nx] = '#';
-			}
+			up(ny, nx);
+			right(ny, nx);
+			down(ny, nx);
+			left(ny, nx);
 		}
 	}
 	int temp = 0;
@@ -349,4 +143,36 @@ void sol() {
 		}
 	}
 	ans = min(ans, temp);
+}
+
+void left(int ny, int nx) {
+	while (true) {
+		nx -= 1;
+		if (nx < 0 || arr[ny][nx] == '6') break;
+		if (arr[ny][nx] == '0') arr[ny][nx] = '#';
+	}
+}
+
+void right(int ny, int nx) {
+	while (true) {
+		nx += 1;
+		if (nx >= M || arr[ny][nx] == '6') break;
+		if (arr[ny][nx] == '0') arr[ny][nx] = '#';
+	}
+}
+
+void up(int ny, int nx) {
+	while (true) {
+		ny -= 1;
+		if (ny < 0 || arr[ny][nx] == '6') break;
+		if (arr[ny][nx] == '0') arr[ny][nx] = '#';
+	}
+}
+
+void down(int ny, int nx) {
+	while (true) {
+		ny += 1;
+		if (ny >= N || arr[ny][nx] == '6') break;
+		if (arr[ny][nx] == '0') arr[ny][nx] = '#';
+	}
 }
