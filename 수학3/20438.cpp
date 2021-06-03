2d23eac6cb8561f1ff0ba5710e72af5dbcaa8966
woongseob <55429912+woongseob12@users.noreply.github.com>
2021-06-03 13:20:53 +0900

    출석체크
    알고리즘: 누적합
    1. 졸고 있는 학생 체크를 위한 배열 
    - index 처리하여 시간복잡도O(1)로 접근
    2. 출석체크하기
    - 입장번호는 무조건 배수에게 다 줌(조는 학생 뒤에 전달이 안되는것이 아님!!!)
    - 대신 처음 시작이 조는 학생이라면 조는 학생 뒤는 모두 전달이 안됨
    3. 구간 개수 구하기
    - 누적합을 이용하여 구하기(일일이 더하면 시간초과)
    - 누적합에서 저장하는 원소는 해당 구간까지 출석이 되지 않는 학생
    - C++의 특성(true = 1, false = 0)을 이용하여 !studnet[i]을 더해서 계산

---
+++ 수학3/20438.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool student[5003], sleep[5003];
+int attend[5003];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, K, Q, M;
+	cin >> N >> K >> Q >> M;
+		
+	for (int i = 0; i < K; i++) {
+		int val;
+		cin >> val;
+		sleep[val] = true;
+	}
+
+	// √‚ºÆ
+	for (int i = 0; i < Q; i++) {
+		int val;
+		cin >> val;
+		if (sleep[val]) continue;
+		for (int j = val; j <= N + 2; j += val) {
+			if (!sleep[j]) student[j] = true;
+		}
+	}
+
+	for (int i = 3; i <= N + 2; i++) {
+		attend[i] = attend[i - 1] + (!student[i]);
+	}
+
+	for (int i = 3; i <= N + 2; i++) {
+		//if (!student[i]) cout << i << " ";
+		cout << i << ": " << attend[i] << endl;
+	}
+	for (int i = 0; i < M; i++) {
+		int S, E;
+		cin >> S >> E;
+		cout << attend[E] - attend[S - 1] << "\n";
+	}
+}
