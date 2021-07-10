40fef941d85e516869e50d3f36e0ce1eaccd4ca1
woongseob <woongseob12@naver.com>
2021-07-10 23:27:13 +0900

    전자레인지
    알고리즘: 그리디
    1. 가장 오래 걸리는 시간부터 나눠주기
    2. 문제의 난이도 보단 단순성을 제거하는 방식에 초점을 두어 클린 코드를 작성하는데 중점을 두었음

---
+++ 그리디 알고리즘/BOJ10162.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class BOJ10162 {
+	
+	static int[] microwave = {300, 60, 10};
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int time = Integer.parseInt(br.readLine());
+		if(time % 10 != 0) {
+			System.out.println("-1");
+		} else {
+			int[] arr = new int[3];
+			for(int i = 0; i < microwave.length; i++) {
+				arr[i] = time / microwave[i];
+				time %= microwave[i];
+			}
+			
+			for(int i = 0; i < microwave.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+}
