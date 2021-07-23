2e2ce60ca5c90ce057faf1781ee85607c68f43df
woongseob <woongseob12@naver.com>
2021-07-23 23:03:40 +0900

    등수 매기기
    알고리즘: 그리디
    1. 오름차순으로 정렬 후 하나씩 넣어주기
    2. 데이터형의 범위 생각하기

---
+++ 그리디 알고리즘/2012.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N, ans;
+vector<int> rankArr;
+void input();
+void solve();
+void output();
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	input();
+	solve();
+	output();
+}
+
+void input() {
+	cin >> N;
+	rankArr.resize(N);
+	for (int i = 0; i < N; i++) {
+		cin >> rankArr[i];
+	}
+}
+
+void solve() {
+	sort(rankArr.begin(), rankArr.end());
+	for (int i = 0; i < N; i++) {
+		ans += abs(rankArr[i] - (i + 1));
+	}
+}
+
+void output() {
+	cout << ans;
+}
